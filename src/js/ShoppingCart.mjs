580e70a8993884ad145f4a28daf3eb324c2467a9
ErickHurtad0 <ehurtadocabrales@byupathway.edu>
2026-07-13 22:21:00 -0600

    Cart, header and footer

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,44 @@
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+
+function cartItemTemplate(item) {
+  return `
+<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>
+`;
+}
+
+export default class ShoppingCart {
+  constructor(listElement) {
+    this.listElement = listElement;
+  }
+
+  init() {
+    const list = getLocalStorage("so-cart") || [];
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    if (list.length === 0) {
+      this.listElement.innerHTML = "<li>Your cart is empty.</li>";
+      return;
+    }
+
+    renderListWithTemplate(
+      cartItemTemplate,
+      this.listElement,
+      list
+    );
+  }
+}
