190ef50bbbf7d552216ac2bb6f2e0a79bd0978cf
ErickHurtad0 <ehurtadocabrales@byupathway.edu>
2026-07-14 20:34:16 -0600

    Duplicate items in cart

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -5,7 +5,7 @@ function cartItemTemplate(item) {
 <li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -13,7 +13,7 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <<p class="cart-card__quantity">Qty: ${item.quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>
 `;
